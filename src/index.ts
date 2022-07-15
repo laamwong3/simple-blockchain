d6cbc6bccdd9e1520ef0195ffa3f88b2abe72dff
Laam Wong <laamwong3@gmail.com>
2022-07-16 07:44:08 +0800

    create mining reward

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,89 @@
 import SHA256 from "crypto-js/sha256";
 
+class Transaction {
+  fromAddress: string;
+  toAddress: string;
+  amount: number;
+
+  constructor(fromAddress: string, toAddress: string, amount: number) {
+    this.fromAddress = fromAddress;
+    this.toAddress = toAddress;
+    this.amount = amount;
+  }
+}
+
 class Block {
-  index: number;
   timestamp: string;
-  data: string;
+  transaction: {};
   previousHash: string;
   currentHash: string;
+  nonce: number;
 
-  constructor(
-    index: number,
-    timestamp: string,
-    data: string,
-    previousHash: string
-  ) {
-    this.index = index;
+  constructor(timestamp: string, transaction: {}, previousHash: string = "") {
     this.timestamp = timestamp;
-    this.data = data;
+    this.transaction = transaction;
     this.previousHash = previousHash;
     this.currentHash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash = (): string =>
     SHA256(
-      this.index.toString() + this.previousHash + JSON.stringify(this.data)
+      this.previousHash + JSON.stringify(this.transaction) + this.nonce
     ).toString();
+
+  mine = (difficulity: number) => {
+    while (
+      this.currentHash.substring(0, difficulity) !==
+      Array(difficulity + 1).join("0")
+    ) {
+      this.nonce++;
+      this.currentHash = this.calculateHash();
+      console.log(this.currentHash);
+    }
+    console.log(`Block mined at ${this.currentHash}`);
+  };
 }
 
 class Blockchain {
   chain: Block[];
+  difficulty: number;
+  pendingTransaction: {}[];
+  miningReward: number;
+
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
+    this.pendingTransaction = [];
+    this.miningReward = 100;
   }
 
   createGenesisBlock = (): Block =>
-    new Block(0, new Date().toString(), "Genesis Block", "0");
+    new Block(new Date().toString(), "Genesis Block", "0");
+
+  getLatestBlock = (): Block => this.chain[this.chain.length - 1];
+
+  minePendingTransaction = () => {};
+
+  // addBlock = (newBlock: Block) => {
+  //   newBlock.previousHash = this.getLatestBlock().currentHash;
+  //   newBlock.mine(this.difficulty);
+  //   // newBlock.currentHash = newBlock.calculateHash();
+  //   this.chain.push(newBlock);
+  // };
+
+  validateChain = (): boolean => {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.currentHash !== currentBlock.calculateHash())
+        return false;
+
+      if (currentBlock.previousHash !== previousBlock.currentHash) return false;
+    }
+    return true;
+  };
 }
+
+export { Block, Blockchain };
---
+++ src/test.ts
@@ -0,0 +1,15 @@
+import { Block, Blockchain } from "./index";
+
+let myChain = new Blockchain();
+
+// myChain.addBlock(new Block(1, new Date().toString(), { hello: "hi" }));
+// myChain.addBlock(new Block(2, new Date().toString(), "test"));
+// console.log(myChain.validateChain());
+// myChain.chain[1].data = "hey";
+// myChain.chain[1].currentHash = myChain.chain[1].calculateHash();
+// console.log(myChain.validateChain());
+// console.log(JSON.stringify(myChain, null, 2));
+// console.log(Array(5).join("0"));
+
+// const str = "abcdef";
+// console.log(str.substring(0, 4));
